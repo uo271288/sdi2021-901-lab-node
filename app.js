42e38822678adcf79e2b3e6f419e188d35486646
UO271288 <uo271288@uniovi.es>
2021-03-30 00:22:52 +0200

    SDI–2021-901-sesión7-Ejercicio-complementario1

--- app.js
+++ app.js
@@ -2,25 +2,22 @@
 let express = require('express');
 let app = express();
 
-let swig = require('swig');
-//Rutas/controladores por lógica
-require("./routes/rusuarios.js")(app, swig);
-require("./routes/rcanciones.js")(app, swig);
-
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static('public'));
 
+let swig = require('swig');
+//Rutas/controladores por lógica
+require("./routes/rusuarios.js")(app, swig);
+require("./routes/rcanciones.js")(app, swig);
+require("./routes/rautores.js")(app, swig);
+
 // Variables
 app.set('port', 8081);
 
-//Rutas/controladores por lógica
-require("./routes/rusuarios.js")(app); // (app, param1, param2, etc.)
-require("./routes/rcanciones.js")(app); // (app, param1, param2, etc.)
-
 // lanzar el servidor
 app.listen(app.get('port'), function () {
-    console.log('Servidor activo');
+    console.log('Servidor activo http://localhost:8081/');
 });
---
+++ routes/rautores.js
@@ -0,0 +1,52 @@
+module.exports = function (app, swig) {
+    app.get('/autores/agregar', function (req, res) {
+        let respuesta = swig.renderFile('views/autores-agregar.html', {});
+        res.send(respuesta);
+    });
+
+    app.post("/autor", function (req, res) {
+        let respuesta = "Autor agregado: <br>";
+        if (typeof (req.body.nombreAutor) != "undefined")
+            respuesta += "Nombre: " + req.body.nombreAutor + "<br>";
+        else
+            respuesta += "Nombre no enviado en la petición <br>";
+        if (typeof (req.body.grupo) != "undefined")
+            respuesta += "Grupo: " + req.body.grupo + "<br>";
+        else
+            respuesta += "Grupo no enviado en la petición <br>";
+        if (typeof (req.body.rol) != "undefined")
+            respuesta += "Rol: " + req.body.rol + "<br>";
+        else
+            respuesta += "Rol no enviado en la petición <br>";
+        res.send(respuesta);
+    });
+
+    app.get("/autores", function (req, res) {
+        let autores = [{
+            "nombre": "Amaia Montero",
+            "grupo": "La Oreja de Van Gogh",
+            "rol": "Cantante"
+        }, {
+            "nombre": "Xabi San Martín",
+            "grupo": "La Oreja de Van Gogh",
+            "rol": "Teclista"
+        }, {
+            "nombre": "Billie Joe Armstrong",
+            "grupo": "Green Day",
+            "rol": "Cantante"
+        }, {
+            "nombre": "Mike Dirnt",
+            "grupo": "Green Day",
+            "rol": "Bajista"
+        }];
+        let respuesta = swig.renderFile('views/autores.html', {
+            //vendedor: 'Tienda de canciones',
+            autores: autores
+        });
+        res.send(respuesta);
+    });
+
+    app.get('/autores(/*)?', function (req, res) {
+        res.redirect("/autores");
+    })
+};
