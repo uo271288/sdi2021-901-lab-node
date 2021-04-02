ce3d58006d2c3a65a10f81d0e133b9a484b15bcc
UO271288 <uo271288@uniovi.es>
2021-04-02 21:36:13 +0200

    SDI-2021-101-8.2-Arquitectura para el acceso a datos.

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 // Módulos
 let express = require('express');
 let app = express();
+
 let mongo = require('mongodb');
 let swig = require('swig');
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+let gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app, mongo);
+
 app.use(express.static('public'));
 
 // Variables
 app.set('port', 8081);
 app.set('db', 'mongodb://admin:sdi@tiendamusica-shard-00-00.xkin3.mongodb.net:27017,tiendamusica-shard-00-01.xkin3.mongodb.net:27017,tiendamusica-shard-00-02.xkin3.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-dgpspz-shard-0&authSource=admin&retryWrites=true&w=majority');
 //Rutas/controladores por lógica
-require("./routes/rusuarios.js")(app, swig);
-require("./routes/rcanciones.js")(app, swig, mongo);
+require("./routes/rusuarios.js")(app, swig, gestorBD);
+require("./routes/rcanciones.js")(app, swig, gestorBD);
+require("./routes/rautores.js")(app, swig);
 
 // lanzar el servidor
 app.listen(app.get('port'), function () {
--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -1,4 +1,4 @@
-module.exports = function (app, swig, mongo) {
+module.exports = function (app, swig, gestorBD) {
     app.get("/canciones", function (req, res) {
         let canciones = [{
             "nombre": "Blank space",
@@ -19,24 +19,14 @@ module.exports = function (app, swig, mongo) {
 
     app.post("/cancion", function (req, res) {
         let cancion = {
-            nombre: req.body.nombre,
-            genero: req.body.genero,
+            nombre: req.body.nombre, genero: req.body.genero,
             precio: req.body.precio
-        }
-        // Conectarse
-        mongo.MongoClient.connect(app.get('db'), function (err, db) {
-            if (err) {
-                res.send("Error de conexión: " + err);
+        } // Conectarse
+        gestorBD.insertarCancion(cancion, function (id) {
+            if (id == null) {
+                res.send("Error al insertar canción");
             } else {
-                let collection = db.collection('canciones');
-                collection.insertOne(cancion, function (err, result) {
-                    if (err) {
-                        res.send("Error al insertar " + err);
-                    } else {
-                        res.send("Agregada id: " + result.ops[0]._id);
-                    }
-                    db.close();
-                });
+                res.send("Agregada la canción ID: " + id);
             }
         });
     });
