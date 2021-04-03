d32e550d0a14b1d3661c582a92ad9edb73d138a8
UO271288 <uo271288@uniovi.es>
2021-04-03 03:44:58 +0200

    SDI-2021-901-8.3-Subida a ficheros y gestión de colecciones.

--- modules/gestorBD.js
+++ modules/gestorBD.js
@@ -3,6 +3,22 @@ module.exports = {
     init: function (app, mongo) {
         this.mongo = mongo;
         this.app = app;
+    }, obtenerCanciones: function (criterio, funcionCallback) {
+        this.mongo.MongoClient.connect(this.app.get('db'), function (err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                let collection = db.collection('canciones');
+                collection.find(criterio).toArray(function (err, canciones) {
+                    if (err) {
+                        funcionCallback(null);
+                    } else {
+                        funcionCallback(canciones);
+                    }
+                    db.close();
+                });
+            }
+        });
     },
     insertarCancion: function (cancion, funcionCallback) {
         this.mongo.MongoClient.connect(this.app.get('db'), function (err, db) {
--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -26,7 +26,25 @@ module.exports = function (app, swig, gestorBD) {
             if (id == null) {
                 res.send("Error al insertar canción");
             } else {
-                res.send("Agregada la canción ID: " + id);
+                if (req.files.portada != null) {
+                    var imagen = req.files.portada;
+                    imagen.mv('public/portadas/' + id + '.png', function (err) {
+                        if (err) {
+                            res.send("Error al subir la portada");
+                        } else {
+                            if (req.files.audio != null) {
+                                let audio = req.files.audio;
+                                audio.mv('public/audios/' + id + '.mp3', function (err) {
+                                    if (err) {
+                                        res.send("Error al subir el audio");
+                                    } else {
+                                        res.send("Agregada id: " + id);
+                                    }
+                                });
+                            }
+                        }
+                    });
+                }
             }
         });
     });
@@ -34,5 +52,38 @@ module.exports = function (app, swig, gestorBD) {
     app.get('/canciones/agregar', function (req, res) {
         let respuesta = swig.renderFile('views/bagregar.html', {});
         res.send(respuesta);
-    })
+    });
+
+    app.get("/tienda", function (req, res) {
+        let criterio = {};
+        if (req.query.busqueda != null) {
+            criterio = {"nombre": {$regex: ".*" + req.query.busqueda + ".*"}};
+        }
+        gestorBD.obtenerCanciones(criterio, function (canciones) {
+            if (canciones == null) {
+                res.send("Error al listar ");
+            } else {
+                let respuesta = swig.renderFile('views/btienda.html',
+                    {
+                        canciones: canciones
+                    });
+                res.send(respuesta);
+            }
+        });
+    });
+
+    app.get('/cancion/:id', function (req, res) {
+        let criterio = {"_id": gestorBD.mongo.ObjectID(req.params.id)};
+        gestorBD.obtenerCanciones(criterio, function (canciones) {
+            if (canciones == null) {
+                res.send("Error al recuperar la canción.");
+            } else {
+                let respuesta = swig.renderFile('views/bcancion.html',
+                    {
+                        cancion: canciones[0]
+                    });
+                res.send(respuesta);
+            }
+        });
+    });
 };
