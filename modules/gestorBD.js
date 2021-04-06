a588840f917af89d0762cbaae5a76984b4a4396d
UO271288 <uo271288@uniovi.es>
2021-04-06 03:28:04 +0200

    SDI-2021-901-9.5-Sistema de paginación.

--- modules/gestorBD.js
+++ modules/gestorBD.js
@@ -164,5 +164,23 @@ module.exports = {
                 });
             }
         });
+    }, obtenerCancionesPg: function (criterio, pg, funcionCallback) {
+        this.mongo.MongoClient.connect(this.app.get('db'), function (err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                let collection = db.collection('canciones');
+                collection.count(function (err, count) {
+                    collection.find(criterio).skip((pg - 1) * 4).limit(4).toArray(function (err, canciones) {
+                        if (err) {
+                            funcionCallback(null);
+                        } else {
+                            funcionCallback(canciones, count);
+                        }
+                        db.close();
+                    });
+                });
+            }
+        })
     }
 };
--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -55,22 +55,52 @@ module.exports = function (app, swig, gestorBD) {
     });
 
     app.get("/tienda", function (req, res) {
-        let criterio = {};
-        if (req.query.busqueda != null) {
-            criterio = {"nombre": {$regex: ".*" + req.query.busqueda + ".*"}};
-        }
-        gestorBD.obtenerCanciones(criterio, function (canciones) {
-            if (canciones == null) {
-                res.send("Error al listar ");
-            } else {
-                let respuesta = swig.renderFile('views/btienda.html',
-                    {
-                        canciones: canciones
-                    });
-                res.send(respuesta);
+            let criterio = {};
+            if (req.query.busqueda != null) {
+                criterio = {"nombre": {$regex: ".*" + req.query.busqueda + ".*"}};
             }
-        });
-    });
+
+            let pg = parseInt(req.query.pg); // Es String !!!
+            if (req.query.pg == null) { // Puede no venir el param
+                pg = 1;
+            }
+            gestorBD.obtenerCancionesPg(criterio, pg, function (canciones, total) {
+                if (canciones == null) {
+                    res.send("Error al listar ");
+                } else {
+                    let ultimaPg = total / 4;
+                    if (total % 4 > 0) { // Sobran decimales
+                        ultimaPg = ultimaPg + 1;
+                    }
+                    let paginas = []; // paginas mostrar
+                    for (let i = pg - 2; i <= pg + 2; i++) {
+                        if (i > 0 && i <= ultimaPg) {
+                            paginas.push(i);
+                        }
+                    }
+                    let respuesta = swig.renderFile('views/btienda.html', {
+                        canciones: canciones,
+                        paginas: paginas,
+                        actual: pg
+                    });
+                    res.send(respuesta);
+                }
+            });
+
+            /*
+            gestorBD.obtenerCanciones(criterio, function (canciones) {
+                if (canciones == null) {
+                    res.send("Error al listar ");
+                } else {
+                    let respuesta = swig.renderFile('views/btienda.html',
+                        {
+                            canciones: canciones
+                        });
+                    res.send(respuesta);
+                }
+            });*/
+        }
+    );
 
     app.get('/cancion/:id', function (req, res) {
         let criterio = {"_id": gestorBD.mongo.ObjectID(req.params.id)};
