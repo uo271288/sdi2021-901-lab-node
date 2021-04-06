fa569066f5ebb9c1e6d851292c8dc19639ced855
UO271288 <uo271288@uniovi.es>
2021-04-06 03:07:07 +0200

    SDI-2021-901-9.4-Sistema de compras.

--- app.js
+++ app.js
@@ -30,6 +30,8 @@ routerUsuarioSession.use(function (req, res, next) {
 //Aplicar routerUsuarioSession
 app.use("/canciones/agregar", routerUsuarioSession);
 app.use("/publicaciones", routerUsuarioSession);
+app.use("/cancion/comprar", routerUsuarioSession);
+app.use("/compras", routerUsuarioSession);
 
 //routerAudios
 let routerAudios = express.Router();
@@ -41,7 +43,14 @@ routerAudios.use(function (req, res, next) {
         if (req.session.usuario && canciones[0].autor == req.session.usuario) {
             next();
         } else {
-            res.redirect("/tienda");
+            let criterio = {usuario: req.session.usuario, cancionId: mongo.ObjectID(idCancion)};
+            gestorBD.obtenerCompras(criterio, function (compras) {
+                if (compras != null && compras.length > 0) {
+                    next();
+                } else {
+                    res.redirect("/tienda");
+                }
+            });
         }
     })
 });
