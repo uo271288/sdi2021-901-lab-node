3e068086754578d1329c2e068793b5cad2b06098
uo271288 <uo271288@uniovi.es>
2021-04-16 02:21:03 +0200

    SDI-2021-901-10.5-SW Consumir SW desde NodeJS.

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -131,20 +131,45 @@ module.exports = function (app, swig, gestorBD) {
                         if (canciones[0].autor == req.session.usuario || (compras.length > 0 && compras[0].usuario == req.session.usuario))
                             comprado = true
                         if (comentarios == null) {
-                            let respuesta = swig.renderFile('views/bcancion.html',
-                                {
-                                    comprado: comprado,
-                                    cancion: canciones[0]
-                                });
-                            res.send(respuesta);
+                            let configuracion = {
+                                url: "https://www.freeforexapi.com/api/live?pairs=EURUSD",
+                                method: "get",
+                                headers: {"token": "ejemplo",}
+                            }
+                            let rest = app.get("rest");
+                            rest(configuracion, function (error, response, body) {
+                                console.log("cod: " + response.statusCode + " Cuerpo :" + body);
+                                let objetoRespuesta = JSON.parse(body);
+                                let cambioUSD = objetoRespuesta.rates.EURUSD.rate; // nuevo campo "usd"
+                                canciones[0].usd = cambioUSD * canciones[0].precio;
+                                let respuesta = swig.renderFile('views/bcancion.html',
+                                    {
+                                        comprado: comprado,
+                                        cancion: canciones[0]
+                                    });
+                                res.send(respuesta);
+                            })
                         } else {
-                            let respuesta = swig.renderFile('views/bcancion.html',
-                                {
-                                    comprado: comprado,
-                                    cancion: canciones[0],
-                                    comentarios: comentarios
-                                });
-                            res.send(respuesta);
+                            let configuracion = {
+                                url: "https://www.freeforexapi.com/api/live?pairs=EURUSD",
+                                method: "get",
+                                headers: {"token": "ejemplo",}
+                            }
+                            let rest = app.get("rest");
+                            rest(configuracion, function (error, response, body) {
+                                console.log("cod: " + response.statusCode + " Cuerpo :" + body);
+                                let objetoRespuesta = JSON.parse(body);
+                                let cambioUSD = objetoRespuesta.rates.EURUSD.rate; // nuevo campo "usd"
+                                canciones[0].usd = (cambioUSD * canciones[0].precio).toFixed(2);
+                                let respuesta = swig.renderFile('views/bcancion.html',
+                                    {
+                                        comprado: comprado,
+                                        cancion: canciones[0],
+                                        comentarios: comentarios
+                                    });
+                                res.send(respuesta);
+                            })
+
                         }
                     })
 
